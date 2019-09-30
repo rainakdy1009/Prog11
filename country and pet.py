019007e506f3c486cac26f2b2705d75aed26ba9e
rainakdy1009 <55250114+rainakdy1009@users.noreply.github.com>
2019-09-30 09:09:29 -0700

    Delete country and pet.py

--- country and pet.py
+++
@@ -1,8 +0,0 @@
-country = input("Where are you from?: ")
-pet = input("What do you raise as your pet?: ")
-if country == "Canada" and (pet == "Moose" or pet == "Beaver"):
-    answer = input("Do you play hockey too?: ")
-    print("Okay")
-    
-else:
-    print("Canada is a good country to go on a trip.\nTry to raise Moose or Beaver too")
